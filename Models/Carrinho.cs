15508387cb2c3009a57c5fa53ca9bef12313c85a
Brun0 <brunodev41@gmail.com>
2025-02-11 10:09:03 -0300

    Finalização da implantação API DevSteam

---
+++ Models/Carrinho.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DevSteamAPI.Models
+{
+    public class Carrinho
+    {
+        public Guid CarrinhoId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public Decimal? Total { get; set; } = 0;
+        public bool? Finalizado { get; set; } = false;
+    }
+}
---
+++ Models/ItemCarrinho.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSteamAPI.Models
+{
+    public class ItemCarrinho
+    {
+        public Guid ItemCarrinhoId { get; set; }
+        public Guid CarrinhoId { get; set; }
+        public Guid JogoId { get; set; }
+        [Required(ErrorMessage = "A quantidade é obrigatória")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero")]
+        public int Quantidade { get; set; }
+        [Required(ErrorMessage = "o valor é obrigatória")]
+        [Range(0.01, 9999.99, ErrorMessage = "O valor deve ser maior que zero e menor que 9999,99")]
+        public Decimal Valor { get; set; }
+        public decimal? Total { get; set; }
+    }
+}
